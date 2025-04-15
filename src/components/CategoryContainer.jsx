8226f5a3f560364a3dfccc846324154cd7f15134
“[Nathaniel <[omokhodionn@gmail.com]>
2025-04-15 00:18:52 -0800

    update

--- src/components/CategoryContainer.jsx
+++ src/components/CategoryContainer.jsx
@@ -6,25 +6,29 @@ const categories = [
     id: 1,
     name: "Technology",
     description: "Latest trends in technology",
-    imageUrl: " public/image/p1.jpg",
+    imageUrl:
+      " https://images.pexels.com/photos/725255/pexels-photo-725255.jpeg?auto=compress&cs=tinysrgb&w=600",
   },
   {
     id: 2,
     name: "Health",
     description: "Health and wellness tips",
-    imageUrl: " public/image/p3.jpg",
+    imageUrl:
+      " https://images.pexels.com/photos/40568/medical-appointment-doctor-healthcare-40568.jpeg?auto=compress&cs=tinysrgb&w=600",
   },
   {
     id: 3,
     name: "Business",
     description: "Business and finance news",
-    imageUrl: " public/image/p2.jpg",
+    imageUrl:
+      " https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg?auto=compress&cs=tinysrgb&w=600",
   },
   {
     id: 4,
     name: "Lifestyle",
     description: "Lifestyle and culture",
-    imageUrl: " public/image/pexels-zeke-syrcle-2150599202-31225680 (2).jpg",
+    imageUrl:
+      " https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg?auto=compress&cs=tinysrgb&w=600",
   },
 ];
 
